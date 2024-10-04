66c57dd1030eb6fe121711a0c32d277c6a7cabae
“[firstname <lyhwan5356@gmail.com>
2024-10-04 16:45:32 +0900

    Initial commit

---
+++ src/main/java/bbs/Bbs.java
@@ -0,0 +1,83 @@
+package bbs;
+
+public class Bbs {
+	private int bbsID;
+	private String bbsTitle;
+	private String userID;
+	private String bbsDate;
+	private String bbsContent;
+	private int bbsAvailable;
+	/**
+	 * @return the bbsID
+	 */
+	public int getBbsID() {
+		return bbsID;
+	}
+	/**
+	 * @param bbsID the bbsID to set
+	 */
+	public void setBbsID(int bbsID) {
+		this.bbsID = bbsID;
+	}
+	/**
+	 * @return the bbsTitle
+	 */
+	public String getBbsTitle() {
+		return bbsTitle;
+	}
+	/**
+	 * @param bbsTitle the bbsTitle to set
+	 */
+	public void setBbsTitle(String bbsTitle) {
+		this.bbsTitle = bbsTitle;
+	}
+	/**
+	 * @return the userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	/**
+	 * @return the bbsDate
+	 */
+	public String getBbsDate() {
+		return bbsDate;
+	}
+	/**
+	 * @param bbsDate the bbsDate to set
+	 */
+	public void setBbsDate(String bbsDate) {
+		this.bbsDate = bbsDate;
+	}
+	/**
+	 * @return the bbsContent
+	 */
+	public String getBbsContent() {
+		return bbsContent;
+	}
+	/**
+	 * @param bbsContent the bbsContent to set
+	 */
+	public void setBbsContent(String bbsContent) {
+		this.bbsContent = bbsContent;
+	}
+	/**
+	 * @return the bbsAvailable
+	 */
+	public int getBbsAvailable() {
+		return bbsAvailable;
+	}
+	/**
+	 * @param bbsAvailable the bbsAvailable to set
+	 */
+	public void setBbsAvailable(int bbsAvailable) {
+		this.bbsAvailable = bbsAvailable;
+	}
+	
+}
